f7f2b0d45205a7884d69478fdfde104886db487e
CancuiW <can.cui@wayne.edu>
2023-09-02 21:46:27 -0700

    finished

--- api/shippers/shippers-middleware.js
+++ api/shippers/shippers-middleware.js
@@ -1,12 +1,43 @@
+const Shipper=require('./shippers-model') 
+
 module.exports = {
   checkId,
   checkPayload,
 }
 
-function checkId(req, res, next) {
-  next()
-}
+ async function checkId(req, res, next) {
+  try{
+    const shipper=await Shipper.getById(req.params.id)
+    if(shipper){
+      req.shipper=shipper;
+      next()
+    }else{
+      next({status:404,message:`Shipper ${req.params.id} not found`})
+    }
 
+  }catch(err){
+    //catch 是处理在异地发送请求的过程中出现了问题
+    next(err)
+  }
+
+ 
+}
+//since we are not use the async request from database,
+//so we do not need to add "try catch" to analysis this situation of sending requests
 function checkPayload(req, res, next) {
-  next()
+  const { ShipperName, Phone }=req.body
+  if ((ShipperName!==undefined &&
+    typeof ShipperName==='string'&&
+    ShipperName.trim().length) ||
+     (Phone !== undefined &&
+      typeof Phone === 'string' &&
+      Phone.trim().length)){
+      req.ShipperName=ShipperName.trim()
+      req.Phone = Phone.trim()
+      next()
+    }else{
+      next({status:422,message:'Phone and name are required'})
+    }
+  
+  
 }
--- api/shippers/shippers-model.js
+++ api/shippers/shippers-model.js
@@ -1,3 +1,6 @@
+const db=require('./../../data/db-config')
+
+
 module.exports = {
   get,
   getById,
@@ -7,21 +10,43 @@ module.exports = {
 }
 
 async function get() {
-  return 'get wired'
+  // .select('shippername','phone') --->returns only this two atributes in the final array
+  const result= await db('shippers')
+                       // .select('shippername','phone')
+  return result
 }
 
-async function getById() {
-  return 'getById wired'
+async function getById(id) {
+  //写法1:const result=await db('shippers').where({shipperID:id}) 
+  //写法2:因为只有一个attribute 所以有另一种写法  const result=await db('shippers').where(shipperID，id) 
+  //if the id is not in the table,it will return [],so we can destructure(解构) this array that will return anything 
+  //for instance :
+  // const [result] = await db('shippers').where('shipperID', id )
+  // return result
+  //another way is to add '.first()' at the end of the line to solve the problem of returning []
+  const result = await db('shippers').where('shipperID', id).first()
+   return result
+
 }
 
-async function create() {
-  return 'create wired'
+async function create(newshipper) {
+  const [newIndexOfItem]=await db('shippers').insert(newshipper)
+  //return [6] --->6 is the index of new item
+  // so we need to use the getById() to get this new item
+  const result= await getById(newIndexOfItem)
+  return result
 }
 
-async function update() {
-  return 'update wired'
+async function update(updateId,newItem) {
+   await db('shippers').where('shipperID', updateId ).update(newItem)
+   //return the count of changeItem ,so we need to use the getById() to get this new item
+  const result = await getById(updateId)
+  return result
 }
 
-async function remove() {
-  return 'delete wired'
+async function remove(id) {
+  const result = await getById(id)
+  await db('shippers').where('shipperID', id).del()
+  
+  return result
 }
